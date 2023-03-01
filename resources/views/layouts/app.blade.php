6050c5687db03e70e0fb249ad1265138e33a9833
eric <ericarc99@gmail.com>
2023-03-01 16:23:55 +0800

    樣式調整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,11 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'LaraBBS') - Laravel 進階教程</title>
+  <title>@yield('title', 'LaraBBS 9') - LaraBBS 9</title>
 
   <!-- Styles -->
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  <!-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
 
