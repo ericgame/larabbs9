19b2155a6b5b6d63cdaa8ac76c701d1cfcb91075
eric <ericarc99@gmail.com>
2023-03-02 17:23:24 +0800

    生成用戶認證代碼

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,53 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', 'PagesController@root')->name('root');
+
+/*
+  // 用戶身份驗證相關的路由
+  GET|HEAD   login .............. login › Auth\LoginController@showLoginForm
+  POST       login .............. Auth\LoginController@login
+  POST       logout ............. logout › Auth\LoginController@logout
+
+  // 用戶註冊相關路由
+  GET|HEAD   register .................. register › Auth\RegisterController@showRegistrationForm
+  POST       register .................. Auth\RegisterController@register
+
+  // 密碼重置相關路由
+  POST       password/email ..... password.email › Auth\ForgotPasswordController@sendResetLinkEmail
+  GET|HEAD   password/reset ..... password.request › Auth\ForgotPasswordController@showLinkRequestForm
+  POST       password/reset ..... password.update › Auth\ResetPasswordController@reset
+  GET|HEAD   password/reset/{token} .... password.reset › Auth\ResetPasswordController@showResetForm
+
+  // 再次確認密碼（重要操作前提示）
+  GET|HEAD   password/confirm ... password.confirm › Auth\ConfirmPasswordController@showConfirmForm
+  POST       password/confirm ... Auth\ConfirmPasswordController@confirm
+*/
+// Auth::routes(); //用下面方式取代
+
+// 用戶身份驗證相關的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// 用用戶註冊相關路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// 密碼重置相關路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// 再次確認密碼（重要操作前提示）
+Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
+
+// Email 認證相關路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 
 
