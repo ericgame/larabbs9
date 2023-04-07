e186a058a67c83c727ee3ac8fe81b976ea68b6f1
eric <ericarc99@gmail.com>
2023-04-07 16:05:24 +0800

    生成話題骨架

--- routes/web.php
+++ routes/web.php
@@ -61,3 +61,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 */
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+//帖子
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
