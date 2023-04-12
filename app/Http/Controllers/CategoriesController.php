dcc4926ad584820e5694006c467b1fbf9fbebf44
eric <ericarc99@gmail.com>
2023-04-12 15:53:27 +0800

    話題排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,13 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         // 讀取與"分類id"關聯的話題，一頁20條訊息
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)
+                        ->where('category_id', $category->id)
+                        ->with('user', 'category')   // 預加載防止 N+1 問題
+                        ->paginate(20);
 
         // 傳參變量"話題"和"分類"到模板中
         return view('topics.index', compact('topics', 'category'));
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -19,4 +19,30 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的數據讀取邏輯
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        // 當話題有新回覆時，我們將編寫邏輯來更新話題模型的 reply_count 屬性，
+        // 此時會自動觸發框架對數據模型 updated_at 時間戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照創建時間排序
+        return $query->orderBy('created_at', 'desc');
+    }
 }
