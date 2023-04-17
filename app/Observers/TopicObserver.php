cb44cef20aeab3b9212cf450715b232fc45e350b
eric <ericarc99@gmail.com>
2023-04-17 17:42:26 +0800

    修復 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,10 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        // HTMLPurifier 過濾輸入的資料，避免 XSS 攻擊
+        $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 產生"文章摘要"，用於SEO
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
