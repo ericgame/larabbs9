b86ba4cdd326e8c2a511f671014afdd4a20102af
eric <ericarc99@gmail.com>
2023-05-29 17:28:42 +0800

    顯示用戶角色

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -22,9 +22,10 @@ class UserResource extends JsonResource
 
         $data = parent::toArray($request);
 
-        $data['last_actived_at'] = (string)$this->last_actived_at;
+        $data['last_actived_at'] = (string) $this->last_actived_at;
         $data['bound_phone'] = $this->resource->phone ? true : false;
         $data['bound_wechat'] = ($this->resource->weixin_unionid || $this->resource->weixin_openid) ? true : false;
+        $date['roles'] = RoleResource::collection($this->whenLoaded('roles'));
 
         return $data;
     }
