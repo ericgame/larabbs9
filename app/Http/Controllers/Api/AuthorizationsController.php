560b79601f84d8ad784ac3da951db307b66afd0f
eric <ericarc99@gmail.com>
2023-06-08 00:18:02 +0800

    Passport 第三方登錄

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -15,68 +15,6 @@ class AuthorizationsController extends AccessTokenController
         return $this->issueToken($request)->setStatusCode(201);
     }
 
-    public function socialStore($type, SocialAuthorizationRequest $request)
-    {
-        $driver = \Socialite::create($type);
-
-        try {
-            if ($code = $request->code) {
-                $oauthUser = $driver->userFromCode($code);
-            } else {
-                $tokenData['access_token'] = $request->access_token;
-
-                // 微信需要增加 openid
-                if ($type == 'wechat') {
-                    $driver->withOpenid($request->openid);
-                }
-
-                $oauthUser = $driver->userFromToken($request->access_token);
-            }
-        } catch (\Exception $e) {
-            throw new AuthenticationException('參數錯誤，未獲取用戶信息');
-        }
-
-        if (!$oauthUser->getId()) {
-            throw new AuthenticationException('參數錯誤，未獲取用戶信息');
-        }
-
-        switch ($type) {
-            case 'wechat':
-                // 當 $oauthUser->getRaw()['unionid'] 不為 null，return $oauthUser->getRaw()['unionid']，否則 return null
-                $unionid = $oauthUser->getRaw()['unionid'] ?? null;
-
-                if ($unionid) {
-                    $user = User::where('weixin_unionid', $unionid)->first();
-                } else {
-                    $user = User::where('weixin_openid', $oauthUser->getId())->first();
-                }
-
-                // 沒有用戶，默認創建一個用戶
-                if (!$user) {
-                    $user = User::create([
-                        'name' => $oauthUser->getNickname(),
-                        'avatar' => $oauthUser->getAvatar(),
-                        'weixin_openid' => $oauthUser->getId(),
-                        'weixin_unionid' => $unionid,
-                    ]);
-                }
-
-                break;
-        }
-
-        $token = auth('api')->login($user);
-        return $this->respondWithToken($token)->setStatusCode(201);
-    }
-
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
-        ]);
-    }
-
     public function update(ServerRequestInterface $request)
     {
         return $this->issueToken($request);
